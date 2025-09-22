b70897d9eefaaffcd574ac4975a315e8b8ba85b6
Frederik Rüütel <kfreqk@gmail.com>
2025-09-22 15:28:30 +0300

    Lisatud Tom Bombadill

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("Hello World!")
+let num = 1;
+console.log(num);
+console.log(typeof num);
+
+num = 1.2;
+console.log(num);
+console.log(typeof num);
+
+let text = "Hello Jaskar";
+console.log(text);
+console.log(typeof text);
+
+text = "H";
+console.log(text);
+console.log(typeof text);
+
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+
+let array = [1, 2, 'Jaskar', 'on', 'Neeger', true, [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+
+let obj = {
+    name: 'Tom Bombadill',
+    age: '67',
+    heirColor: 'Red',
+    isMale: true,
+    isHomoSex: false,
+    BLMSupporter: true,
+    isPedofile: true,
+};
+
+console.log(obj);
